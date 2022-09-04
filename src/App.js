470d060338efc91f180528cb24aa61729a2d043d
Helen Shatynska <94306487+niftywebsite@users.noreply.github.com>
2022-09-04 19:10:20 +0300

    created project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    <h1>Nifty Dictionary</h1>
-  </div>;
+  return (
+    <div className="App">
+      <h1>Nifty Dictionary</h1>
+    </div>
+  );
 }
 
 export default App;
