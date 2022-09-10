24276aa0b2db11d05be305d018978fdd0ca883f7
Helen Shatynska <94306487+niftywebsite@users.noreply.github.com>
2022-09-10 15:09:31 +0300

    added ReactAudioPlayer component

--- src/Audio.js
+++ src/Audio.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Audio(props) {
   if (props.data) {
-    return <a href={props.data}>Listen</a>;
+    return (
+      <div>
+        <ReactAudioPlayer src={props.data} autoPlay controls />
+      </div>
+    );
   }
 }
